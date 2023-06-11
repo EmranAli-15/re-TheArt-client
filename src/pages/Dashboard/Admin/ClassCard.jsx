228bb3ba97c6d4c1fa816189e03b43c7215e99ca
EmranAli-15 <mohammademranali15@gmail.com>
2023-06-12 02:49:27 +0600

    popular instructor done

--- src/pages/Dashboard/Admin/ClassCard.jsx
+++ src/pages/Dashboard/Admin/ClassCard.jsx
@@ -18,26 +18,51 @@ const ClassCard = ({ card }) => {
             });
     }
 
+    const handleMessage = (e) => {
+        // e.preventDefault();
+        // const message = e.target.message.value;
+        // const id = e.target.id.value;
+        // console.log(message, id);
+        // e.target.reset();
+        console.log(e._id, e.instructorEmail);
+    }
+
     return (
-        <div className="card card-compact w-full bg-base-100 shadow-xl">
-            <figure><img className='h-44' src={image} /></figure>
-            <div className="card-body">
-                <div className='flex items-center justify-between'>
-                    <h2 className="card-title">{name}</h2>
-                    <h3 className='md:pr-4 text-lg'>Price: $ {price}</h3>
-                </div>
-                <p>Instructor Name : {instructorName}</p>
-                <p>Instructor Email : {instructorEmail}</p>
-                <h3>Available Seats : {seats}</h3>
-                <h3>{status}</h3>
-                <hr />
-                <div className='flex justify-between items-center'>
-                    <div className='flex items-center gap-x-4'>
-                        <button onClick={() => handleApproved(_id)} className={`${status === 'pending' ? 'text-green-500' : ''} btn btn-sm`} disabled={status === 'approved'}><BsCheckLg size={30}></BsCheckLg></button>
-                        <button className='btn btn-sm text-red-500' disabled={status === 'approved'}><BsXLg size={30}></BsXLg></button>
+        <div>
+            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+
+            <div className="modal">
+                <div className="modal-box">
+                    <input name="id" type="text" defaultValue={_id} readOnly placeholder="Type here" className="input input-bordered input-info w-full" />
+                    <textarea name="message" className="textarea textarea-info w-full" placeholder="Bio"></textarea>
+                    <div className='text-right'>
+                        <button onClick={() => handleMessage(_id)}>ok</button>
                     </div>
-                    <div>
-                        <button className='btn btn-sm text-blue-500'><BsSendCheckFill size={30}></BsSendCheckFill></button>
+                </div>
+                <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+            </div>
+
+            <div className="card card-compact w-full bg-base-100 shadow-xl">
+                <figure><img className='h-44' src={image} /></figure>
+                <div className="card-body">
+                    <div className='flex items-center justify-between'>
+                        <h2 className="card-title">{name}</h2>
+                        <h3 className='md:pr-4 text-lg'>Price: $ {price}</h3>
+                    </div>
+                    <p>Instructor Name : {instructorName}</p>
+                    <p>Instructor Email : {instructorEmail}</p>
+                    <h3>Available Seats : {seats}</h3>
+                    <h3>{status}</h3>
+                    <hr />
+                    <div className='flex justify-between items-center'>
+                        <div className='flex items-center gap-x-4'>
+                            <button onClick={() => handleApproved(_id)} className={`${status === 'pending' ? 'text-green-500' : ''} btn btn-sm`} disabled={status === 'approved'}><BsCheckLg size={30}></BsCheckLg></button>
+                            <button className='btn btn-sm text-red-500' disabled={status === 'approved'}><BsXLg size={30}></BsXLg></button>
+                        </div>
+                        <div>
+                            <label htmlFor="my_modal_7" onClick={()=>handleMessage(card)} className='btn btn-sm text-blue-500'><BsSendCheckFill size={30}></BsSendCheckFill></label>
+                            {/* <button className='btn btn-sm text-blue-500'><BsSendCheckFill size={30}></BsSendCheckFill></button> */}
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/pages/Home/PopularInstructor/PopularInstructor.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import axios from 'axios';
+
+const PopularInstructor = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const [instructor, setInstructor] = useState([]);
+
+    axiosSecure('/popularInstructors')
+        .then(res => {
+            setInstructor(res.data);
+        })
+    return (
+        <div>
+            <h1 className='text-lg md:text-4xl mt-20 mb-8 font-serif uppercase font-medium text-center'>Popular Instructor</h1>
+            <div className='my-8 grid md:grid-cols-3 gap-4'>
+                {
+                    instructor.map(man =>
+                        <div key={man._id} className="card w-full bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src={man.photo} alt="Shoes" className="rounded-xl h-52 w-56" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{man.name}</h2>
+                                <p>{man.email}</p>
+                            </div>
+                        </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularInstructor;
